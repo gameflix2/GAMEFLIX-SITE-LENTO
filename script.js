eed3d13bb6dff62ce4018c82e68cf4bf16660ce4
gameflix2 <oarsenaldegames@gmail.com>
2026-04-27 23:19:35 -0300

    Update script.js

--- script.js
+++ script.js
@@ -78,7 +78,7 @@ function setBannerVideo(videoSrc) {
     iframe.style.cssText = [
       'position:absolute',
       'top:50%', 'left:50%',
-      'width:300%', 'height:300%',
+      'width:100%', 'height:100%',
       'transform:translate(-50%,-50%)',
       'border:none',
       'pointer-events:none'
